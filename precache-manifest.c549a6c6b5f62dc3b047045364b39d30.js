aa85d78421fd5962ea639012fb04dec750593868
Juandlc <juniorelpapa09@hotmail.com>
2020-09-02 19:45:45 -0500

    Updates

---
+++ precache-manifest.c549a6c6b5f62dc3b047045364b39d30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bfe3e4ea417ef92407c69af3c3c7190",
+    "url": "/tinder-clone/index.html"
+  },
+  {
+    "revision": "39c62d4f97498d367c26",
+    "url": "/tinder-clone/static/css/main.056d1ac7.chunk.css"
+  },
+  {
+    "revision": "ad8f28e8e796335c9219",
+    "url": "/tinder-clone/static/js/2.82e4f482.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/tinder-clone/static/js/2.82e4f482.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39c62d4f97498d367c26",
+    "url": "/tinder-clone/static/js/main.c742b742.chunk.js"
+  },
+  {
+    "revision": "de86c0115f6746d23e21",
+    "url": "/tinder-clone/static/js/runtime-main.acd27e62.js"
+  }
+]);
